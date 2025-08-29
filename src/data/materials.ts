11fbbf9198d068dd116b60b28e9699a9bffb0d5e
odouglasrocha <odouglasrocha@gmail.com>
2025-08-29 14:21:56 -0300

    atualização bacon 420

--- src/data/materials.ts
+++ src/data/materials.ts
@@ -3,7 +3,7 @@ import { Material } from '../types/production';
 export const materialsData: Material[] = [
   { "Codigo": "300061751", "Material": "TORCIDA BACON 35GX26 PP", "Gramagem": "0,035", "Und": 26, "Caixas": 150, "PPm": 65 },
   { "Codigo": "300061750", "Material": "TORCIDA BACON 60GX24 PP", "Gramagem": "0,060", "Und": 24, "Caixas": 135, "PPm": 65 },
-  { "Codigo": "300061635", "Material": "TORCIDA BACON 420GX16 PP", "Gramagem": "0,420", "Und": 16, "Caixas": 16, "PPm": 30 },
+  { "Codigo": "300061635", "Material": "TORCIDA BACON 420GX16 PP", "Gramagem": "0,420", "Und": 16, "Caixas": 336, "PPm": 30 },
 
   { "Codigo": "300061778", "Material": "TORCIDA CHURRASCO 60GX24 PP", "Gramagem": "0,060", "Und": 24, "Caixas": 135, "PPm": 75 },
   { "Codigo": "300061779", "Material": "TORCIDA CHURRASCO 35GX26 PP", "Gramagem": "0,035", "Und": 26, "Caixas": 150, "PPm": 75 },
